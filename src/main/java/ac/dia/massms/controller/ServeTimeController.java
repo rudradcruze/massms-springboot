ff3fbc39f9ef0577c9f8e9b3acdfb6b7b9952ed5
rudradcruze <francisrudra@gmail.com>
2023-06-25 18:42:55 +0600

    serveTime view created

--- src/main/java/ac/dia/massms/controller/ServeTimeController.java
+++ src/main/java/ac/dia/massms/controller/ServeTimeController.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -22,5 +26,22 @@ public class ServeTimeController {
         model.addAttribute("mealTimes", mealTimes);
 
         return "meal_times";
+    }
+
+    @PostMapping("/meal/time/new/save")
+    public String saveServeTime(@ModelAttribute("serveTime") ServeTime serveTime, RedirectAttributes attributes, Principal principal) {
+
+        if (principal == null) { return "redirect:/login"; }
+
+        String dbIdentifier = serveTimeService.getByIdentifier(serveTime.getIdentifier()).getIdentifier();
+
+        if (dbIdentifier.equals(serveTime.getIdentifier())) {
+            attributes.addFlashAttribute("error", "Can't insert. This Identifier is already exist!");
+            return "redirect:/meal/time/new";
+        }
+        else
+            attributes.addFlashAttribute("success", serveTime.getIdentifier() + " is successfully created.");
+
+        return "redirect:/meal/time";
     }
 }
