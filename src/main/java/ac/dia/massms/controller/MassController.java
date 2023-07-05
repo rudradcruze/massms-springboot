ec46174ebf7bdeef0710e88aba020473663287ad
rudradcruze <francisrudra@gmail.com>
2023-07-05 10:50:15 +0600

    mass edit page data show

--- src/main/java/ac/dia/massms/controller/MassController.java
+++ src/main/java/ac/dia/massms/controller/MassController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -52,6 +53,14 @@ public class MassController {
             model.addAttribute("error", "This url is already exist! Please put another url");
             return "new_mass";
         }
+    }
+
+    @RequestMapping("/mass/edit/{id}")
+    public ModelAndView newMassEditPage(@PathVariable("id") long id) {
+        Mass mass = massService.getById(id);
+        ModelAndView modelAndView = new ModelAndView("edit_mass");
+        modelAndView.addObject("mass", mass);
+        return modelAndView;
     }
 
     @RequestMapping("/mass/status/{id}")
